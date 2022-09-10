d0affafe5485c6770bc4df6e33b7b64290a3e1b0
GAYATHTRI <110927478+GAYATHTRI@users.noreply.github.com>
2022-09-10 16:36:02 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,40 @@
+let myProfile = {
+    "name": "Gayathri G",
+    "email": "mcagayathri@gmail.com",
+    "phone": "7871256666",
+    "degree": "BCA",
+    "education": [
+        {
+            "SSlC": "Government Higher Sec School",
+            "Hsc": "Government Higher Sce School",
+        }
+    ],
+
+    "location": {
+        "Address": "9/121 Near bank kavarkalptty.",
+        "postcode": "636111",
+        "city": "salem",
+        "state": "Tamilnadu",
+        "country": "India",
+    },
+    "skills": [
+        {
+            "name": "javascript",
+            "level": "beginner",
+        }
+          
+    ],
+    "languages": [
+        {
+            "language": "Tamil,English",
+        }
+    ],
+    "interests": [
+        {
+            "Hobbies": "google searching",
+        }
+    ]
+}
+        
+console.log(myProfile);
+
